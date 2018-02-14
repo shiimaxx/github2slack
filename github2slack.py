ff598ba0dbf2504c4c5d635c47ba772aaf1e937b
shiimaxx <yoshima@shiimaxx.com>
2018-02-14 21:43:53 +0900

    Add test

---
+++ github2slack.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def fetch_unread_notifications():
+    pass
---
+++ tests/test_github2slack.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+import unittest
+
+from github2slack import fetch_unread_notifications
+
+
+NOTIFICATIONS = {
+    'github2slack': [
+        {
+            'subject': {'title': 'pre-release'},
+            'unread': True,
+            'updated_at': datetime(2018, 1, 1, 0, 0),
+            'url': 'https:example.com/notification/threads/100000000'
+        },
+        {
+            'repository': {'name': 'github2slack'},
+            'subject': {'title': 'Feature/logging'},
+            'unread': True,
+            'updated_at': datetime(2018, 1, 1, 1, 25),
+            'url': 'https:example.com/notification/threads/100000001'
+        },
+        {
+            'repository': {'name': 'github2slack'},
+            'subject': {'title': 'Refactoring'},
+            'unread': False,
+            'updated_at': datetime(2018, 1, 5, 10, 5),
+            'url': 'https:example.com/notification/threads/100000004'
+        }],
+    'pocket': [
+        {
+            'subject': {'title': 'Add test'},
+            'unread': True,
+            'updated_at': datetime(2018, 1, 2, 13, 23),
+            'url': 'https:example.com/notification/threads/100000002'
+        }],
+    'dynamic-route53': [
+        {
+            'subject': {'title': 'development'},
+            'unread': False,
+            'updated_at': datetime(2018, 1, 5, 3, 45),
+            'url': 'https:example.com/notification/threads/100000003'
+        }]
+}
+
+
+class TestGithub2Slack(unittest.TestCase):
+    def test_fetch_unread_notification(self):
+        unread_notifications = fetch_unread_notifications()
+        self.assertEqual(2, len(unread_notifications['github2slack']))
+        self.assertEqual(1, len(unread_notifications['pocket']))
+        self.assertNotIn('dynamic-route53', unread_notifications.keys())
